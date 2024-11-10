703bf29a72c3196d0b09ce635f5b960a935f004c
DavidMetalor <davidmetalor46@gmail.com>
2024-11-10 18:55:16 +0100

    OOP part 2

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ class Student:
 
  
 obj = Student()
+
+class student:
+  grade = 10
+  name = "Jade"
+  def introduction(self):
+    print("This is a student")
+    def details(self):
+      print("Student's name is", self.name)
+      print("Student's grade is", self.grade)
+
+      obj = student()
+      obj.introduction()
+      obj.details()
+      
